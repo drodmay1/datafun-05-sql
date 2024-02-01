c290f664fc570be9311dba68eda32b029dda196f
David <drodmay@gmail.com>
2024-02-01 09:11:11 -0600

    docstring created

--- david_sql.py
+++ david_sql.py
@@ -1,3 +1,19 @@
+"""
+Project for Module 5 Learn databases and SQL
+
+This project ntegrates Python and SQL, focusing on database interactions using SQLite.
+introduces logging, creating and managing databases and SQL operations
+ncluding queries with joins, filters, and aggregations.
+
+Author: David Rodriguez Mayorquin
+Date: 2/1/2024
+
+Dependencies:
+Pandas
+Pyarrow
+"""
+
+
 import sqlite3
 import pandas as pd
 connection = sqlite3.connect('books.db')
