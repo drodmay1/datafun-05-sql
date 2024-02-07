e9671e5315c20c2423e54fe7ede73cc6b11be2a8
David <drodmay@gmail.com>
2024-02-06 19:57:46 -0500

    new updates

---
+++ Proj5_SQL_Python.py
@@ -0,0 +1,53 @@
+"""
+Project for Module 5 Learn databases and SQL
+
+This project integrates Python and SQL, focusing on database interactions using SQLite.
+introduces logging, creating and managing databases and SQL operations
+including queries with joins, filters, and aggregations.
+
+Author: David Rodriguez Mayorquin
+Date: 2/6/2024
+
+Dependencies:
+Pandas
+Pyarrow
+"""
+
+import sqlite3
+import pandas as pd
+from pathlib import Path
+import logging
+import csv
+import uuid
+import pyarrow
+
+# Configure logging to write to a file, appending new logs to the existing file
+logging.basicConfig(filename='log.txt', level=logging.DEBUG, filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
+
+logging.info("Program started")
+logging.info("Program ended")
+
+def execute_sql_from_file(db_filepath, sql_file):
+    with sqlite3.connect(db_filepath) as conn:
+        with open(sql_file, 'r') as file:
+            sql_script = file.read()
+        conn.executescript(sql_script)
+        print(f"Executed SQL from {sql_file}")
+
+def main():
+    db_filepath = '/Users/davidrodriguez/Documents/NWMO/datafun-05-sql/datafun-05-sql/project.db'
+
+    # Create database schema and populate with data
+    execute_sql_from_file(db_filepath, 'create_tables.sql')
+    execute_sql_from_file(db_filepath, 'insert_records.sql')
+    execute_sql_from_file(db_filepath, 'update_records.sql')
+    execute_sql_from_file(db_filepath, 'delete_records.sql')
+    execute_sql_from_file(db_filepath, 'query_aggregation.sql')
+    execute_sql_from_file(db_filepath, 'query_filter.sql')
+    execute_sql_from_file(db_filepath, 'query_sorting.sql')
+    execute_sql_from_file(db_filepath, 'query_group_by.sql')
+    execute_sql_from_file(db_filepath, 'query_join.sql')
+
+    logging.info("All SQL operations completed successfully")
+
+
